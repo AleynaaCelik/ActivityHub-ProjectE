e5bea9f923a16bad6e168b48e30597969b3e0871
eda özdemir <88721437+eda426@users.noreply.github.com>
2024-05-29 23:55:02 +0300

    [Feature]Infrastructure oluşturuldu

---
+++ ActivitiyHub.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+﻿using ActivityHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitiyHub.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<UserActivity> UserActivities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure relationships, constraints, etc.
+            modelBuilder.Entity<UserActivity>()
+                .HasKey(ua => new { ua.UserId, ua.ActivityId });
+
+            modelBuilder.Entity<UserActivity>()
+                .HasOne(ua => ua.User)
+                .WithMany(u => u.UserActivities)
+                .HasForeignKey(ua => ua.UserId);
+
+            modelBuilder.Entity<UserActivity>()
+                .HasOne(ua => ua.Activity)
+                .WithMany(a => a.UserActivities)
+                .HasForeignKey(ua => ua.ActivityId);
+        }
+    }
+}
---
+++ ActivitiyHub.Infrastructure/Data/Repositories/ActivityRepository.cs
@@ -0,0 +1,18 @@
+﻿using ActivityHub.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitiyHub.Infrastructure.Data.Repositories
+{
+    public class ActivityRepository : Repository<Activity>, IActivityRepository
+    {
+        public ActivityRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        // Add any activity-specific methods here
+    }
+}
